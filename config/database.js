4d1f84f1fa149772c14ed82170978decc276de3e
stozuka <stozuka@gmail.com>
2017-06-26 19:48:23 +0800

    setup database

---
+++ config/database.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'vote',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'vote',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'vote',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  }
+}
